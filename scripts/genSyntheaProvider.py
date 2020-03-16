52c59d54e6bca82733b8779ffd244d82e7f3814e
xxshambm <ext.michael.shamberger@tieto.com>
2020-03-16 15:36:34 +0200

    try first run

--- scripts/genSyntheaProvider.py
+++ scripts/genSyntheaProvider.py
@@ -1,13 +1,23 @@
-# generate synthea providers based on data extracted from openstreetmaps
 import os
+from dotenv import load_dotenv
 import pandas as pd
 import time
 import ModelSyntheaPandas
 
 countries = ["BE", "BG", "CZ", "DK", "DE", "EE", "IE", "ES", "FR", "HR", "IT", "LV", "LT", "LU", "HU", "MT", "NL", "AT", "PL", "PT", "RO", "SI", "SK", "FI", "SE", "NO", "GB"]
 
+# ------------------------
+# load env
+# ------------------------
+load_dotenv(verbose=True)
+
 # set output directory
-BASE_OUTPUT_DIRECTORY='./'
+# Path to the directory containing the input healthsites files
+BASE_INPUT_DIRECTORY    = os.environ['BASE_INPUT_DIRECTORY']
+# Path to the base directory that provider files will be written to
+BASE_OUTPUT_DIRECTORY    = os.environ['BASE_OUTPUT_DIRECTORY']
+print('BASE_INPUT_DIRECTORY     =' + BASE_INPUT_DIRECTORY)
+print('BASE_OUTPUT_DIRECTORY    =' + BASE_OUTPUT_DIRECTORY)
 
 # load the synthea model
 model_synthea = ModelSyntheaPandas.ModelSyntheaPandas()
@@ -20,8 +30,12 @@ for country in countries:
         # map OSM data synthea data
         # create hospitals.csv and urgent_care_facilities.csv
         # urgent_care_facilities has emergency=yes
+        OUTPUT_DIRECTORY =  os.path.join(BASE_OUTPUT_DIRECTORY,country.lower())
+        OUTPUT_DIRECTORY = OUTPUT_DIRECTORY + '/src/main/resources/providers'
+        if not os.path.exists(OUTPUT_DIRECTORY):
+            os.makedirs(OUTPUT_DIRECTORY)
         df = pd.DataFrame(columns=model_synthea.model_schema['hospitals'].keys())
-        df.to_csv(os.path.join(BASE_OUTPUT_DIRECTORY,'hospitals.csv'), mode='w', header=True, index=True)
+        df.to_csv(os.path.join(OUTPUT_DIRECTORY,'hospitals.csv'), mode='w', header=True, index=True)
         df['name'] = hospitals['name']
         df['LAT'] =  hospitals['lat']
         df['LON'] = hospitals['lon']
@@ -35,7 +49,7 @@ for country in countries:
         df.to_csv(os.path.join(BASE_OUTPUT_DIRECTORY,'hospitals.csv'), mode='w', header=True, index=True)
         # create urgent_care_facilities by filtering on emergency
         df = df.loc[df['emergency'].str.lower() == 'yes']
-        df.to_csv(os.path.join(BASE_OUTPUT_DIRECTORY,'urgent_care_facilities.csv'), mode='w', header=True, index=True)
+        df.to_csv(os.path.join(OUTPUT_DIRECTORY,'urgent_care_facilities.csv'), mode='w', header=True, index=True)
     else:
         print("File " + file + " does not exist")
 
@@ -54,6 +68,6 @@ for country in countries:
         if 'addr:housenumber' in clinics.columns and 'addr:street' in clinics.columns:
             #df['address'] = clinics['addr:housenumber'] + " " + clinics['addr:street']
             df['address'] = clinics['addr:street']
-        df.to_csv(os.path.join(BASE_OUTPUT_DIRECTORY,'primary_care_facilities.csv'), mode='w', header=True, index=True)
+        df.to_csv(os.path.join(OUTPUT_DIRECTORY,'primary_care_facilities.csv'), mode='w', header=True, index=True)
     else:
         print("File " + file + " does not exist")
