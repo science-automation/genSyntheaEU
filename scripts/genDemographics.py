b9eed4a8ef04942125e99021a9ff082316c04173
xxshambm <ext.michael.shamberger@tieto.com>
2020-03-18 11:35:27 +0200

    first try for demographics

---
+++ scripts/genDemographics.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import os
+import zipfile
+from dotenv import load_dotenv
+
+# ------------------------
+# load env
+# ------------------------
+load_dotenv(verbose=True)
+
+# set output directory
+# Path to the directory containing the city data
+BASE_INPUT_DIRECTORY    = os.environ['BASE_INPUT_DIRECTORY']
+# Path to the base directory that demographhic files will be written to
+BASE_OUTPUT_DIRECTORY   = os.environ['BASE_OUTPUT_DIRECTORY']
+
+# load the synthea model
+model_synthea = ModelSyntheaPandas.ModelSyntheaPandas()
+
+# load the world cities into a dataframe
+cities = pd.read_csv(BASE_INPUT_DIRECTORY + '/cities500.txt', dtype=model_data.model_schema['geoname'], sep='\t', encoding = "utf-8")
+
+# list of countries to be processed.  No FI since we have better data
+countries= ["BE", "BG", "CY", "CZ", "DK", "DE", "EE", "GR", "IE", "ES", "FR", "HR", "IT", "LV", "LT", "LU", "HU", "MT", "NL", "AT", "PL", "PT", "RO", "SI", "SK", "SE", "NO", "GB"]
+
+for country in countries:
+    print("Processing: " + country)
+    OUTPUT_DIRECTORY =  os.path.join(BASE_OUTPUT_DIRECTORY,country.lower())
+    OUTPUT_DIRECTORY = OUTPUT_DIRECTORY + '/src/main/resources/geography'
+    if not os.path.exists(OUTPUT_DIRECTORY):
+        os.makedirs(OUTPUT_DIRECTORY)
+    # create synthea demographic df
+    citieslocal = pd.DataFrame(columns=model_synthea.model_schema['demographics'].keys())
+    # filter only cities in this country
+    df = citieslocal.loc[citieslocal['country code'] == country]
+    df['NAME'] = citieslocal['name']
+    df['CTYNAME'] = citieslocal['name']
+    df['STNAME'] = citieslocal['admin1 code']
+    df['TOT_POP'] = citieslocal['population']
+    # load the file into a dataframe
+    df.to_csv(OUTPUT_DIRECTORY + '/demographics.csv', mode='w', header=True, index=True)
