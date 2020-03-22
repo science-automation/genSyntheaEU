7671cbfc7b7ebbac2de5082b1071c4acaa6fdf93
xxshambm <ext.michael.shamberger@tieto.com>
2020-03-22 11:49:23 +0200

    fix gb

--- scripts/genPostalCodes.py
+++ scripts/genPostalCodes.py
@@ -42,9 +42,12 @@ for country in countries:
         csvfile = tmppath + "/" + country + ".txt"
         df = pd.read_csv(csvfile, dtype='object', sep='\t', names=columns, header=None)
         # get the region iso code
-        df = pd.merge(df,isodf,left_on='admin_name1', right_on='name', how='left')
-        # change some column names
-        df = df.rename(columns={"admin_name1": "USPS", "admin_code1": "ST", "place_name": "NAME", "postal_code": "ZCTA5", "latitude": "LAT", "longitude": "LON"})
+        if (country=='GB'):
+            df = pd.merge(df,isodf,left_on='admin_name2', right_on='name', how='left')
+            df = df.rename(columns={"admin_name2": "USPS", "isocodem": "ST", "place_name": "NAME", "postal_code": "ZCTA5", "latitude": "LAT", "longitude": "LON"})
+        else:
+            df = pd.merge(df,isodf,left_on='admin_name1', right_on='name', how='left')
+            df = df.rename(columns={"admin_name1": "USPS", "admin_code1": "ST", "place_name": "NAME", "postal_code": "ZCTA5", "latitude": "LAT", "longitude": "LON"})
         # clean up
         df['NAME'] = df['NAME'].str.rstrip()
         # write the zipcodes.csv file
